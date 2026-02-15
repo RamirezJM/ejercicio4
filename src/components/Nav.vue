c31217d207c112a40c6fd51a7a6fb69e239dd1ce
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2026-02-14 23:00:33 -0300

    feat: agregar navbar

---
+++ src/components/Nav.vue
@@ -0,0 +1,37 @@
+<script setup>
+</script>
+<template>
+  <nav>
+    <h1>Centro de Eventos</h1>
+    <ul>
+      <li><a href="#">Nosotros</a></li>
+      <li><a href="#">Eventos</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style>
+nav {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  padding: 1em 2em;
+  background: linear-gradient(rgb(112, 106, 49), rgb(120, 126, 120));
+}
+
+ul{
+  display: flex;
+  list-style-type: none;
+  gap: 2em;
+}
+
+h1 {
+  margin-right: auto;
+}
+
+a {
+  text-decoration: none;
+  color: #323232;
+  font-size: 1.2rem;
+}
+</style>
